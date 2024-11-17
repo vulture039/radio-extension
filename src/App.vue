0f295b13eaa584b23490f28b9ec4547de2de854a
vulture039 <qwertyTF.lab@gmail.com>
2024-11-17 16:52:01 +0900

    add authenticator

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import ProgramList from "./components/ProgramList.vue";
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
 </script>
 
 <template>
   <main>
-    <ProgramList />
+    <authenticator>
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+        <ProgramList />
+      </template>
+    </authenticator>
   </main>
 </template>
 
